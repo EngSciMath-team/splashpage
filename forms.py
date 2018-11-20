0c6be9f4b39a612c55591524ddcaa597017e85a5
Simon Rupp-Greene <siru4581@colorado.edu>
2018-11-20 01:14:51 -0700

    add some styling and columns/formatting/rows stuff to attempt to follow given design. ref: #1

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class IntroForm(FlaskForm):
-    email = StringField('Email Address', validators=[DataRequired()])
-    blurb = TextAreaField('Tell Us About Yourself!', validators=[DataRequired()])
+    email = StringField('Email Address (see notes below)', validators=[DataRequired()])
+    blurb = TextAreaField('Tell us a little about your background or interest in STEM as it relates to socialism, we do not need any personally identifying information.', validators=[DataRequired()])
     submit = SubmitField('Submit')
